9072aebb389f4e0c0b540d562d25fdf15d8d724e
MyNameTsThad <pvpthadgaming@gmail.com>
2022-04-17 21:41:06 +0700

    React Suspense implementation

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,35 @@
-import Accomplishments from '../components/Acomplishments/Accomplishments';
+import {Suspense} from 'react'
+import dynamic from "next/dynamic";
+
 import BgAnimation from '../components/BackgroundAnimation/BackgroundAnimation';
 import Hero from '../components/Hero/Hero';
-import Projects from '../components/Projects/Projects';
-import Technologies from '../components/Technologies/Technologies';
-import AboutMe from '../components/AboutMe/AboutMe';
 import {Layout} from '../layout/Layout';
 import {Section} from '../styles/GlobalComponents';
-import {Head} from "next/document";
-import BlogSection from "../components/Blog/BlogSection";
-import Gallery from "../components/Gallery/Gallery";
+
+const Technologies = dynamic(
+    () => import('../components/Technologies/Technologies'),
+    {suspense: true}
+);
+const Projects = dynamic(
+    () => import('../components/Projects/Projects'),
+    {suspense: true}
+);
+const AboutMe = dynamic(
+    () => import('../components/AboutMe/AboutMe'),
+    {suspense: true}
+);
+const Accomplishments = dynamic(
+    () => import('../components/Acomplishments/Accomplishments'),
+    {suspense: true}
+);
+const Gallery = dynamic(
+    () => import("../components/Gallery/Gallery"),
+    {suspense: true}
+);
+const BlogSection = dynamic(
+    () => import("../components/Blog/BlogSection"),
+    {suspense: true}
+);
 
 const Home = () => {
   return (
@@ -17,12 +38,18 @@ const Home = () => {
           <Hero/>
           <BgAnimation/>
         </Section>
-        <Technologies/>
-        <Projects/>
-        <AboutMe/>
-        <Accomplishments/>
-        <Gallery/>
-        <BlogSection/>
+        <Suspense fallback={<div/>}>
+          <Technologies/>
+          <Projects/>
+        </Suspense>
+        <Suspense fallback={<div/>}>
+          <AboutMe/>
+          <Accomplishments/>
+        </Suspense>
+        <Suspense fallback={<div/>}>
+          <Gallery/>
+          <BlogSection/>
+        </Suspense>
       </Layout>
   );
 }
