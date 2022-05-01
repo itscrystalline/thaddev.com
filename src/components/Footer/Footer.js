2dc24313266a92f93a94e5a9f1a3eb87896a1fdc
MyNameTsThad <pvpthadgaming@gmail.com>
2022-05-01 08:38:01 +0700

    update leetcode stats - 2

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -21,54 +21,59 @@ import useWindowSize from "../../styles/GlobalComponents/WindowSize";
 
 const Footer = () => {
   const {width, height} = useWindowSize();
-  console.log(`width-footer: ${width}`);
-  const navLink = <NavLink fontSize={width < 768 ? (width < 640 ? 14 : 16) : 18} href="https://github.com/MyNameTsThad/thaddev.com/blob/master/README.md">Some Rights Reserved.</NavLink>;
   return (
-      <FooterWrapper>
-        <LinkList>
-          <LinkColumn>
-            <LinkTitle>Facebook</LinkTitle>
-            <LinkItem href={Links[5].link}>Thad Choyrum</LinkItem>
-          </LinkColumn>
-          <LinkColumn>
-            <LinkTitle>LINE</LinkTitle>
-            <LinkItem>@Thad040608</LinkItem>
-          </LinkColumn>
-          <LinkColumn>
-            <LinkTitle>Email</LinkTitle>
-            <LinkItem href={Links[6].link}>real.thad.choyrum@gmail.com</LinkItem>
-          </LinkColumn>
-        </LinkList>
-        <SocialIconsContainer>
-          <CompanyContainer>
-            <Slogan>
-              © {new Date().getFullYear() === 2022 ? "2022" : "2022 - " + new Date().getFullYear()} | ThadDev. {navLink}
-              <br/>
-              Made with <AiFillHeart size="2rem" color="red"/> using <DiReact size="2rem" color="teal"/> <SiNextdotjs size="2rem"/>.
-              <br/>
-              Hosted on <FaRaspberryPi size="2rem" color="red"/> with <FaCloudflare size="2rem" color="orange"/> and&nbsp;<SiNgrok
-              size="2rem" color="blue"/>.
-            </Slogan>
-          </CompanyContainer>
-          <SocialContainer>
-            <SocialIcons href={Links[0].link}>
-              <AiFillGithub size="3rem"/>
-            </SocialIcons>
-            <SocialIcons href={Links[1].link}>
-              <FaHackerrank size="3rem"/>
-            </SocialIcons>
-            <SocialIcons href={Links[2].link}>
-              <SiLeetcode size="3rem"/>
-            </SocialIcons>
-            <SocialIcons href={Links[3].link}>
-              <FaItchIo size="3rem"/>
-            </SocialIcons>
-            <SocialIcons href={Links[4].link}>
-              <FaGooglePlay size="3rem"/>
-            </SocialIcons>
-          </SocialContainer>
-        </SocialIconsContainer>
-      </FooterWrapper>
+    <FooterWrapper>
+      <LinkList>
+        <LinkColumn>
+          <LinkTitle>Facebook</LinkTitle>
+          <LinkItem href={Links[5].link}>Thad Choyrum</LinkItem>
+        </LinkColumn>
+        <LinkColumn>
+          <LinkTitle>LINE</LinkTitle>
+          <LinkItem>@Thad040608</LinkItem>
+        </LinkColumn>
+        <LinkColumn>
+          <LinkTitle>Email</LinkTitle>
+          <LinkItem href={Links[6].link}>real.thad.choyrum@gmail.com</LinkItem>
+        </LinkColumn>
+      </LinkList>
+      <SocialIconsContainer>
+        <CompanyContainer>
+          <Slogan>
+            © {new Date().getFullYear() === 2022 ? "2022" : "2022 - " + new Date().getFullYear()} | ThadDev.&nbsp;
+            {width < 768 ? (width < 640 ?
+                <NavLink fontSize={14} href="https://github.com/MyNameTsThad/thaddev.com/blob/master/README.md">Some
+                  Rights Reserved.</NavLink> :
+                <NavLink fontSize={16} href="https://github.com/MyNameTsThad/thaddev.com/blob/master/README.md">Some
+                  Rights Reserved.</NavLink>) :
+              <NavLink fontSize={18} href="https://github.com/MyNameTsThad/thaddev.com/blob/master/README.md">Some
+                Rights Reserved.</NavLink>}
+            <br/>
+            Made with <AiFillHeart size="2rem" color="red"/> using <DiReact size="2rem" color="teal"/> <SiNextdotjs size="2rem"/>.
+            <br/>
+            Hosted on <FaRaspberryPi size="2rem" color="red"/> with <FaCloudflare size="2rem" color="orange"/> and&nbsp;
+            <SiNgrok size="2rem" color="blue"/>.
+          </Slogan>
+        </CompanyContainer>
+        <SocialContainer>
+          <SocialIcons href={Links[0].link}>
+            <AiFillGithub size="3rem"/>
+          </SocialIcons>
+          <SocialIcons href={Links[1].link}>
+            <FaHackerrank size="3rem"/>
+          </SocialIcons>
+          <SocialIcons href={Links[2].link}>
+            <SiLeetcode size="3rem"/>
+          </SocialIcons>
+          <SocialIcons href={Links[3].link}>
+            <FaItchIo size="3rem"/>
+          </SocialIcons>
+          <SocialIcons href={Links[4].link}>
+            <FaGooglePlay size="3rem"/>
+          </SocialIcons>
+        </SocialContainer>
+      </SocialIconsContainer>
+    </FooterWrapper>
   );
 };
 
