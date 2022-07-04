2c3da9e74c38643fce40f1aa6918f345b66ff314
IWant2Tryhard <62128640+MyNameTsThad@users.noreply.github.com>
2022-07-04 21:49:56 +0700

    Delete src directory

--- src/components/BackgroundAnimation/BackgroundAnimationStyles.js
+++
@@ -1,7 +0,0 @@
-import styled from "styled-components";
-
-export const BGSvg = styled.svg`
-  @media ${(props) => props.theme.breakpoints.md} {
-    display: none;
-  }
-`;
--- src/components/HttpVerb/HttpVerb.js
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-export const HttpVerb = styled.div`
-  background: ${props => props.color};
-  border-radius: 3px;
-  display: inline-block;
-  padding: 3px;
-  align-items: center;
-`;
